04e763f0e832fa3015db81c6da7c11521e3182a6
Jared Delapasse <jareddelapasse@gmail.com>
2023-06-21 18:06:11 -0500

    better styling solution

--- App.tsx
+++ App.tsx
@@ -4,13 +4,23 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontWeight: "bold" }}>
-        Here are some boxes of different colors
-      </Text>
-      <Text style={styles.cyan}>cyan </Text>
-      <Text style={styles.blue}>blue </Text>
-      <Text style={styles.magenta}>magenta </Text>
-      <Text style={styles.orange}>orange </Text>
+      <View>
+        <Text style={{ fontWeight: "bold" }}>
+          Here are some boxes of different colors
+        </Text>
+      </View>
+      <View style={[styles.box, styles.cyan]}>
+        <Text style={styles.boxText}>cyan </Text>
+      </View>
+      <View style={[styles.box, styles.blue]}>
+        <Text style={styles.boxText}>blue </Text>
+      </View>
+      <View style={[styles.box, styles.magenta]}>
+        <Text style={styles.boxText}>magenta </Text>
+      </View>
+      <View style={[styles.box, styles.orange]}>
+        <Text style={styles.boxText}>orange </Text>
+      </View>
       {/* <StatusBar style="auto" /> */}
     </View>
   );
@@ -21,27 +31,32 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
-    // flexDirection: "row",
+    // justifyContent: "center",
+    padding: 16,
+    paddingTop: 40,
+    // paddingHorizontal:
+  },
+  box: {
+    width: "100%",
+    textAlign: "center",
+    marginBottom: 5,
+    alignItems: "center",
+    padding: 10,
+  },
+  boxText: {
+    color: "white",
+    fontWeight: "bold",
   },
   cyan: {
     backgroundColor: "#2aa198",
-    width: "100%",
-    textAlign: "center",
   },
   blue: {
     backgroundColor: "#268bd2",
-    width: "100%",
-    textAlign: "center",
   },
   magenta: {
     backgroundColor: "#d33682",
-    width: "100%",
-    textAlign: "center",
   },
   orange: {
     backgroundColor: "#cb4b16",
-    width: "100%",
-    textAlign: "center",
   },
 });
