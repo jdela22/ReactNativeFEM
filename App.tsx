b119d7cab60cba4050193fdcd5f0ed9ff139df8b
Jared Delapasse <jareddelapasse@gmail.com>
2023-06-21 17:50:28 -0500

    styling challenge

--- App.tsx
+++ App.tsx
@@ -4,8 +4,14 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! Test q </Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontWeight: "bold" }}>
+        Here are some boxes of different colors
+      </Text>
+      <Text style={styles.cyan}>cyan </Text>
+      <Text style={styles.blue}>blue </Text>
+      <Text style={styles.magenta}>magenta </Text>
+      <Text style={styles.orange}>orange </Text>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -16,5 +22,26 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    // flexDirection: "row",
+  },
+  cyan: {
+    backgroundColor: "#2aa198",
+    width: "100%",
+    textAlign: "center",
+  },
+  blue: {
+    backgroundColor: "#268bd2",
+    width: "100%",
+    textAlign: "center",
+  },
+  magenta: {
+    backgroundColor: "#d33682",
+    width: "100%",
+    textAlign: "center",
+  },
+  orange: {
+    backgroundColor: "#cb4b16",
+    width: "100%",
+    textAlign: "center",
   },
 });
